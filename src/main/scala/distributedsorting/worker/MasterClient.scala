f00c5d747f122197b3c7d9072030fd929349d6d3
Hong Chaemin <cmhong@postech.ac.kr>
2025-11-03 01:19:37 +0900

    feat: Define MasterClientTrait for sampling

--- src/main/scala/distributedsorting/worker/MasterClient.scala
+++ src/main/scala/distributedsorting/worker/MasterClient.scala
@@ -1,5 +1,19 @@
 package distributedsorting.worker
 
-trait MasterClient {
-    
+import distributedsorting._
+import distributedsorting.logic.Sampler
+import java.nio.file.Path
+
+import io.grpc.ManagedChannel
+import distributedsorting.MasterServiceGrpc 
+
+type MasterClientStub = MasterServiceGrpc.MasterServiceFutureStub
+
+trait MasterClient { self: Sampler => 
+
+    // Master와의 통신 스텁 및 Worker ID 정의
+    val masterClient: MasterClientStub
+    val workerId: String
+        
+    def runSamplingPhase(inputDirs: Seq[Path]): PartitionInfo = ???
 }
