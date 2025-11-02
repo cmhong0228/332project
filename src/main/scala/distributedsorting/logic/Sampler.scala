5d7153170110f673de44eacf7363d9ec412eb5dc
Hong Chaemin <cmhong@postech.ac.kr>
2025-11-03 01:14:41 +0900

    feat: Implement sampler skeleton code

--- src/main/scala/distributedsorting/logic/Sampler.scala
+++ src/main/scala/distributedsorting/logic/Sampler.scala
@@ -1,5 +1,14 @@
 package distributedsorting.logic
 
 trait Sampler {
-    
+    // key의 길이
+    val KEY_SIZE: Int
+
+    /**
+     * 주어진 확률로 입력 Iterator에서 Key를 샘플링
+     * @param inputIterator 입력 레코드 스트림
+     * @param samplingRatio 마스터로부터 받은 샘플링 확률 (k/n)
+     * @return 샘플링된 Key 리스트 (Seq[Key])
+     */
+    def sampleKeys(inputIterator: Iterator[Record], samplingRatio: Double): Seq[Key] = ???
 }
