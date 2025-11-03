967e4c81fe6bdd0e2a5c3a60cd0bccf16d3f568f
Hong Chaemin <cmhong@postech.ac.kr>
2025-11-04 00:15:12 +0900

    refactor: Decouple sampling logic using specialized traits

--- src/main/scala/distributedsorting/master/MasterServiceImpl.scala
+++ src/main/scala/distributedsorting/master/MasterServiceImpl.scala
@@ -1,9 +1,10 @@
 package distributedsorting.master
 
 import distributedsorting.distributedsorting._
+import distributedsorting.logic._
 import scala.concurrent.{Future, Promise}
 
-class MasterServiceImpl(numWorkers: Int) extends MasterServiceGrpc.MasterService {
+class MasterServiceImpl(val numWorkers: Int) extends MasterServiceGrpc.MasterService with SamplingPolicy with PivotSelector{
 
     // ======================= Sampling =======================
     /*
@@ -13,16 +14,34 @@ class MasterServiceImpl(numWorkers: Int) extends MasterServiceGrpc.MasterService
      * * worker로 부터 Sampling 된 records를 받아 pivots을 구한 후 전송
      */
      
-    private val KEY_LENGTH = ???
-    private val RECORD_LENGTH = ???
+    // key의 길이
+    val KEY_SIZE: Int = ???
+    // memory의 크기
+    val MEMORY_SIZE: Long = ???
+    // 최대 사용할 메모리의 비율
+    val MAX_MEMORY_USAGE_RATIO: Double = ???
+    // 머신 당 받고자 하는 평균적인 바이트 수
+    val BYTES_PER_MACHINE: Long = ???
+    // 머신 마다 넘지 않도록 설계한 바이트 수
+    val MAX_BYTES_PER_MACHINE_DESIGNED: Long = ???
+    // 정렬 시 사용할 ordering
+    val ordering: Ordering[Key] = ???
 
+    /**
+     * [RPC 메서드] Worker가 자신이 가진 레코드 개수를 Master에게 보고할 때 호출
+     * @param request RecordCountReport (Worker ID 및 해당 Worker의 총 레코드 개수 포함)
+     * @return SamplingRatio (계산된 샘플링 확률)을 Worker에게 전송
+     * 내부적으로 SamplingPolicy.calculateSamplingRatio를 호출
+     */
     override def reportRecordCount(request: RecordCountReport): Future[SamplingRatio] = ???
 
+    /**
+     * [RPC 메서드] Worker가 로컬에서 샘플링한 Key 리스트를 Master에게 보낼 때 호출
+     * * Master는 모든 Worker의 샘플을 통합한 후, PivotSelector의 SortSamples와 selectPivots을 호출하여
+     * 파티셔닝 경계 키를 결정
+     * @param request SampleKeyList (Worker ID 및 샘플링된 Key 리스트 포함)
+     * @return SampleDecision (계산된 최종 Pivot Key 리스트를 Worker에게 전송)
+     */
     override def sendSampleKeys(request: SampleKeyList): Future[SampleDecision] = ???
 
-    private def calculateSamplingRatio(numTotalRecords: Long, numSampleRecords: Long): Double = ???
-
-    private def SortSamples(samples: Seq[Key]): Seq[Key] = ???
-
-    private def selectPivots(sortedSamples: Seq[Key], numWorkers: Int): Vector[Key] = ???
 }
--- src/main/scala/distributedsorting/worker/MasterClient.scala
+++ src/main/scala/distributedsorting/worker/MasterClient.scala
@@ -1,12 +1,12 @@
 package distributedsorting.worker
 
 import distributedsorting.distributedsorting._
-import distributedsorting.logic.Sampler
+import distributedsorting.logic._
 import java.nio.file.Path
 import io.grpc.ManagedChannel
 import distributedsorting.distributedsorting.MasterServiceGrpc.MasterServiceStub
 
-trait MasterClient { self: Sampler => 
+trait MasterClient { self: RecordCountCalculator with RecordExtractor => 
     // Master와의 통신 스텁 및 Worker ID 정의
     val masterClient: MasterServiceStub
     val workerId: String
@@ -20,5 +20,11 @@ trait MasterClient { self: Sampler =>
      * pivots: Vector[key]
      */
 
-    def runSamplingPhase(inputDirs: Seq[Path]): Vector[Key] = ???
+    /**
+     * Worker 노드의 샘플링 프로세스를 실행하는 함수
+     * Master와의 2단계 통신(보고 -> 비율 획득, 샘플 전송 -> 피벗 획득)을 조정하고, 최종 피벗을 반환
+     * @param inputDirs Worker가 처리할 데이터가 위치한 로컬 디스크 경로 목록
+     * @return Master로부터 받은, 데이터를 파티셔닝할 최종 경계 Key 벡터 (Pivots)
+     */
+    def executeSampling(inputDirs: Seq[Path]): Vector[Key] = ???
 }
