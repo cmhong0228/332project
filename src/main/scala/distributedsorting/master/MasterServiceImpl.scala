2e3eb2325faee975a01a98498281d2c4ac8a886b
Hong Chaemin <cmhong@postech.ac.kr>
2025-11-11 15:01:02 +0900

    feat: Implement sampling part of MasterServiceImpl

--- src/main/scala/distributedsorting/master/MasterServiceImpl.scala
+++ src/main/scala/distributedsorting/master/MasterServiceImpl.scala
@@ -2,9 +2,16 @@ package distributedsorting.master
 
 import distributedsorting.distributedsorting._
 import distributedsorting.logic._
-import scala.concurrent.{Future, Promise}
+import scala.concurrent.{Future, Promise, ExecutionContext}
+import com.typesafe.config.ConfigFactory
+import distributedsorting.{MasterServiceGrpc, RecordCountReport, SampleKeyList, SamplingRatio, SampleDecision, PartitionInfo, KeyMessage}import java.util.concurrent.ConcurrentLinkedQueue
+import java.util.concurrent.atomic.{AtomicInteger, AtomicLong}
+import scala.jdk.CollectionConverters._
+import com.google.protobuf.ByteString
 
 class MasterServiceImpl(val numWorkers: Int) extends MasterServiceGrpc.MasterService with SamplingPolicy with PivotSelector{
+    private val config = ConfigFactory.load()
+    private val configPath = "distributedsorting"
 
     // ======================= Sampling =======================
     /*
@@ -15,19 +22,27 @@ class MasterServiceImpl(val numWorkers: Int) extends MasterServiceGrpc.MasterSer
      */
      
     // key의 길이
-    val KEY_SIZE: Int = ???
+    val KEY_SIZE: Int = config.getInt(s"$configPath.record-info.key-length")
     // record의 길이
-    val RECORD_SIZE: Int = ???
+    val RECORD_SIZE: Int = config.getInt(s"$configPath.record-info.record-length")
     // memory의 크기
-    val MEMORY_SIZE: Long = ???
+    val MEMORY_SIZE: Long = config.getBytes(s"$configPath.cluster-info.node-info.memory").toLong
     // 최대 사용할 메모리의 비율
-    val MAX_MEMORY_USAGE_RATIO: Double = ???
+    val MAX_MEMORY_USAGE_RATIO: Double = config.getDouble(s"$configPath.sampling-control.max-memory-usage-ratio")
     // 머신 당 받고자 하는 평균적인 바이트 수
-    val BYTES_PER_MACHINE: Long = ???
+    val BYTES_PER_MACHINE: Long = config.getBytes(s"$configPath.sampling-control.bytes-per-machine").toLong
     // 머신 마다 넘지 않도록 설계한 바이트 수
-    val MAX_BYTES_PER_MACHINE_DESIGNED: Long = ???
+    //val MAX_BYTES_PER_MACHINE_DESIGNED: Long = ???
     // 정렬 시 사용할 ordering
-    val ordering: Ordering[Key] = ???
+    val ordering: Ordering[Key] = createRecordOrdering(KEY_SIZE, KEY_SIZE)
+
+    private val reportCounter = new AtomicInteger(0)
+    private val totalRecordSum = new AtomicLong(0)
+    private val samplingRatioPromise = Promise[SamplingRatio]()
+
+    private val sampleCounter = new AtomicInteger(0)
+    private val allSamples = new ConcurrentLinkedQueue[Key]()
+    private val sampleDecisionPromise = Promise[SampleDecision]()
 
     /**
      * [RPC 메서드] Worker가 자신이 가진 레코드 개수를 Master에게 보고할 때 호출
@@ -35,7 +50,21 @@ class MasterServiceImpl(val numWorkers: Int) extends MasterServiceGrpc.MasterSer
      * @return SamplingRatio (계산된 샘플링 확률)을 Worker에게 전송
      * 내부적으로 SamplingPolicy.calculateSamplingRatio를 호출
      */
-    override def reportRecordCount(request: RecordCountReport): Future[SamplingRatio] = ???
+    override def reportRecordCount(request: RecordCountReport): Future[SamplingRatio] = {
+        totalRecordSum.addAndGet(request.totalRecordCount)
+        val currentCount = reportCounter.incrementAndGet()
+
+        if (currentCount == numWorkers) {
+            val totalRecords = totalRecordSum.get()
+            val ratio = calculateSamplingRatio(totalRecords)
+            
+            val response = SamplingRatio(ratio = ratio)
+            
+            samplingRatioPromise.success(response)
+        }
+
+        samplingRatioPromise.future
+    }
 
     /**
      * [RPC 메서드] Worker가 로컬에서 샘플링한 Key 리스트를 Master에게 보낼 때 호출
@@ -44,6 +73,36 @@ class MasterServiceImpl(val numWorkers: Int) extends MasterServiceGrpc.MasterSer
      * @param request SampleKeyList (Worker ID 및 샘플링된 Key 리스트 포함)
      * @return SampleDecision (계산된 최종 Pivot Key 리스트를 Worker에게 전송)
      */
-    override def sendSampleKeys(request: SampleKeyList): Future[SampleDecision] = ???
+    override def sendSampleKeys(request: SampleKeyList): Future[SampleDecision] = {
+        val keysFromWorker: Seq[Key] = request.keys.map(_.value.toByteArray)
+        
+        allSamples.addAll(keysFromWorker.asJava)
+        
+        val currentCount = sampleCounter.incrementAndGet()
+        
+        if (currentCount == numWorkers) {
+            val aggregatedKeys: Seq[Key] = allSamples.asScala.toSeq
+            val sortedKeys = sortSamples(aggregatedKeys)
+            val pivotKeys: Vector[Key] = selectPivots(sortedKeys)
+            val paddedpivots: Vector[Record] = createPaddedPivots(pivotKeys)
+            
+            val protoPivots: Seq[KeyMessage] = pivotKeys.map { keyArray =>
+                KeyMessage(value = ByteString.copyFrom(keyArray))
+            }
+            
+            val partitionInfo = PartitionInfo(pivots = protoPivots)
+            val response = SampleDecision(
+                proceed = true,
+                content = SampleDecision.Content.PartitionInfo(partitionInfo)
+            )
+            /*
+                TODO: sample된 개수 맞지 않는 경우 처리 (일반적으로 일어나진 않지만 예외처리)
+            */
+            
+            sampleDecisionPromise.success(response)
+        }
+
+        sampleDecisionPromise.future
+    }
 
 }
