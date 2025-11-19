5046edfaf16898ada7b64889743b5a06ba4b54a6
Hong Chaemin <cmhong@postech.ac.kr>
2025-11-19 15:12:21 +0900

    feat: Implement basic integration of master

--- src/main/scala/distributedsorting/master/Master.scala
+++ src/main/scala/distributedsorting/master/Master.scala
@@ -1,5 +1,61 @@
 package distributedsorting.master
 
-object Master {
-    
+import scopt.OptionParser
+import java.net.InetAddress
+import io.grpc.ServerBuilder
+
+object Master extends ShutdownController{
+    var server: Server = _
+
+    def main(args: Array[String]): Unit = {
+        implicit val ec: ExecutionContext = ExecutionContext.global
+        
+        MasterArgsParser.parser.parse(args, MasterConfig()) match {
+            case Some(config) if config.numWorkers > 0 =>
+                masterApplication(config.numWorkers)
+            case _ => ()
+        }
+    }
+
+    def masterApplication(numWorkers: Int): Unit = {
+        private val masterIp: String = InetAddress.getLocalHost.getHostAddress 
+        private val masterService = new MasterServiceImpl(numWorkers, this)
+        
+        // 포트 번호 자동 설정
+        server = ServerBuilder.forPort(0)
+        .addService(MasterServiceGrpc.bindService(masterService, ec))
+        .build()
+
+        server.start()
+
+        val masterPort = server.getPort
+        
+        println(s"$masterIp:$masterPort") 
+        
+        // TODO
+        
+        server.awaitTermination()
+    }
+
+    def initiateShutdown(): Unit = {
+        if (server != null) {
+            server.shutdown()
+        }
+    }
+}
+
+case class MasterConfig(numWorkers: Int = 0)
+
+object MasterArgsParser {
+    val parser = new OptionParser[MasterConfig]("master") {
+        
+        head("master", "Distributed Sort Master Node")
+
+        arg[Int]("<# of workers>")
+          .required()
+          .action { (x, c) => c.copy(numWorkers = x) }
+          .text("The fixed number of worker nodes expected to connect.")
+          
+        help("help").text("prints this usage text")
+    }
 }
