938d5710e07d33344958997bdd3511a8094d5a0b
Hong Chaemin <cmhong@postech.ac.kr>
2025-11-28 12:32:39 +0900

    chore: Add log in runSortAndPartition

--- src/main/scala/distributedsorting/logic/InternalSorter.scala
+++ src/main/scala/distributedsorting/logic/InternalSorter.scala
@@ -147,7 +147,7 @@ trait InternalSorter {
    */
   def runSortAndPartition(): List[Path] = {
     val localExecutor = Executors.newFixedThreadPool(optimalThreadCount)
-    println(s"[InternalSorter] optimalThreadcount: $optimalThreadCount, numCores: $numCores, memoryBasedThreadLimit: $memoryBasedThreadLimit")
+    println(s"[InternalSorter] optimalThreadcount: $optimalThreadCount, numCores: $numCores, memoryBasedThreadLimit: $memoryBasedThreadLimit, safeMemoryLimit: $safeMemoryLimit, maxFileSize: $maxFileSize")
   
     implicit val localEc: ExecutionContext = ExecutionContext.fromExecutor(localExecutor)
 
